86c7d753fc13a5db77512e163eb1c8f56d53bd77
Loksli17 <ami0504@mail.ru>
2024-01-25 22:54:07 +1000

    progress

--- components/global/Menu.vue
+++ components/global/Menu.vue
@@ -1,6 +1,8 @@
 
 <script setup lang="ts">
-    
+
+    import Burger from './Burger.vue';
+
 </script>
 
 
@@ -37,17 +39,7 @@
 
         <div class="hidden w-1024:grid ">
 
-            <div>
-                <button class="relative group">
-                    <div class="relative flex overflow-hidden items-center justify-center rounded-full w-[50px] h-[50px] transform transition-all ring-0 group-focus:ring-4 ring-opacity-30 duration-200">
-                        <div class="flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-300 origin-center overflow-hidden">
-                            <div class="bg-white h-[2px] w-7 transform transition-all duration-300 origin-left group-focus:rotate-[42deg]"></div>
-                            <div class="bg-white h-[2px] w-1/2 rounded transform transition-all duration-300 group-focus:-translate-x-10"></div>
-                            <div class="bg-white h-[2px] w-7 transform transition-all duration-300 origin-left group-focus:-rotate-[42deg]"></div>
-                        </div>
-                    </div>
-                </button>
-            </div>
+            <Burger></Burger>
 
         </div>
 
