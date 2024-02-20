a9feca4804bedb44ae67caa4766a68a3802adc2f
Loksli17 <ami0504@mail.ru>
2024-02-20 16:55:01 +1000

    experience: full

--- components/experience/Experience.vue
+++ components/experience/Experience.vue
@@ -5,6 +5,7 @@
     import H3             from '../global/H3.vue';
     import experience     from '../../data/experience';
     import ExperienceItem from './ExperienceItem.vue';
+    import Circle         from '../global/Circle.vue';
 
 </script>
 
@@ -28,7 +29,7 @@
         w-1280:gap-14
     ">
 
-        <div class="grid gap-16 w-1280:gap-12">
+        <div class="grid gap-16 w-1280:gap-8">
 
             <H3 :text="'Опыт работы'"></H3>
             
@@ -46,9 +47,51 @@
         </div>
 
 
-        <div>
+        <div class="grid gap gap-16 w-1280:gap-8 grid-rows-[max-content_1fr]">
 
             <H3 :text="'Образование'"></H3>
+
+            <div class=" bg-white rounded-md shadow relative overflow-hidden gap-8 w-1024:gap-4">
+
+                <div class="overflow-hidden">
+
+                    <ClientOnly>
+
+                        <div :style="{backgroundImage: `url(${useImgUrl(`togu.jpg`)})`}" class="
+                            w-full 
+                            h-[360px]
+                            w-1024:h-[210px] 
+                            bg-center 
+                            bg-cover 
+                            hover:scale-[1.05] 
+                            transition-transform
+                        ">
+                            
+                        </div>
+
+                    </ClientOnly>
+
+                </div>
+
+                <div class="grid items-start p-8 w-1024:p-5 gap-6 w-1024:gap-3">
+
+                    <div class=" text-4xl font-semibold w-1024:text-xl w-750:text-lg">
+                        <h4>ХГТУ [ТОГУ]</h4>
+                    </div>
+                    
+                    <div class=" text-2xl w-1024:text-lg w-750:text-base">
+                        <span>Специальность: <b>Программное обеспечение</b></span>
+                    </div>
+
+                    <div class=" font-light text-lg w-1024:text-base w-750:text-sm text-slate-500">
+                        <span>1991 - 1996 г. Хабаровск</span>
+                    </div>
+
+                </div>
+
+                <Circle :size="170" class=" -bottom-9 -right-9 w-1024:-right-14 w-750:hidden"/>
+
+            </div>
 
         </div>
 
